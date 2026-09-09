380ff637e84da0cbab13eac80bf770be246729c7
raine <raineaveryocampo@gmail.com>
2026-09-09 21:56:09 +0800

    Deleted UI hahah

--- ui/dashboard.py
+++ ui/dashboard.py
@@ -1,7 +1,31 @@
+
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, Q
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QWidget,
+    QLabel,
+    QPushButton,
+    QVBoxLayout,
+    QHBoxLayout,
+    QGridLayout,
+    QFrame,
+    QSpacerItem,
+    QSizePolicy
+)
 
-class Dashboard:
-    pass
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
 
+
+class Dashboard(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+app = QApplication(sys.argv)
+
+window = Dashboard()
+window.show()
+
+sys.exit(app.exec_())
