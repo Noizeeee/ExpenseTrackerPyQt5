00081ad5a23ae1921f9eba0822904aebb9d02ec9
raine <raineaveryocampo@gmail.com>
2026-09-10 13:56:15 +0800

    Dashboard Main Layout

--- ui/dashboard.py
+++ ui/dashboard.py
@@ -30,9 +30,30 @@ class Dashboard(QMainWindow):
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
 
-        # Main horizontal layout
-        main_layout = QHBoxLayout()
+        # Main Vertical layout
+        main_layout = QVBoxLayout()
         central_widget.setLayout(main_layout)
+
+        #Margin
+        main_layout.setContentsMargins(0, 0, 0, 0)
+
+        #Header
+        header = QWidget()
+        header_layout = QHBoxLayout()
+        header.setLayout(header_layout)
+
+        title = QLabel("EXPENSE TRACKER")
+        title.setFont(QFont("Arial", 24, QFont.Bold))
+
+        date = QLabel("Date: mm/dd/yyyy")
+        date.setFont(QFont("Arial", 14))
+
+        header_layout.addWidget(title)
+        header_layout.addStretch()
+        header_layout.addWidget(date)
+        header.setFixedHeight(100)
+        main_layout.addWidget(header)
+
 
         # Left
         sidebar = QWidget()
@@ -42,24 +63,35 @@ class Dashboard(QMainWindow):
 
         #buttons
         dashboard_btn = QPushButton("Dashboard")
-        expenses_btn = QPushButton("Expenses")
-        income_btn = QPushButton("Income")
+        reports_btn = QPushButton("Reports")
+        add_expenses_btn = QPushButton("Add Expenses")
 
         dashboard_btn.setFixedSize(150, 100)
-        expenses_btn.setFixedSize(150, 100)
-        income_btn.setFixedSize(150, 100)
+        reports_btn.setFixedSize(150, 100)
+        add_expenses_btn.setFixedSize(150, 100)
 
         sidebar_layout.addWidget(dashboard_btn)
-        sidebar_layout.addWidget(expenses_btn)
-        sidebar_layout.addWidget(income_btn)
+        sidebar_layout.addWidget(reports_btn)
+        sidebar_layout.addWidget(add_expenses_btn)
 
         sidebar_layout.setAlignment(Qt.AlignCenter)
 
 
         # Right
         content = QWidget()
+        content_layout = QVBoxLayout()
+        content.setLayout(content_layout)
 
-        main_layout.addWidget(sidebar)
-        main_layout.addWidget(content)
+        # Text
+        label = QLabel("Expenses:")
+        label.setAlignment(Qt.AlignTop | Qt.AlignHCenter)
+        content_layout.addWidget(label)
+
+        #Body Layout for Left and Right Display
+        body_layout = QHBoxLayout()
+        body_layout.addWidget(sidebar)
+        body_layout.addWidget(content)
+
+        main_layout.addLayout(body_layout)
 
 
